1987a30db44c5d2d8f7dbbc59332e7fec064442d
dinhan24-04 <phamnguyendinhantk1@gmail.com>
2025-03-28 23:03:11 +0700

    btt7

--- BTT7/src/Main.java
+++ BTT7/src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import javax.swing.SwingUtilities;
 
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new XMLApp().setVisible(true);
+        });
+    }
 }
